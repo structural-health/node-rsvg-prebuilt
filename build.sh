b04181135f9e4aa59be196933d6be7819b1b8e4e
Wolfgang Felbermeier <wf@felbermeier.com>
2018-04-13 09:27:10 +0200

    install nodejs in build container before running npm.. seemed smart to me ;)

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash -xe
 apt-get install librsvg2.0-dev
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 npm install
 npm install -g prebuild
 npm run prebuild
