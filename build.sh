80f1dfe1b68a315a82719039ef090c471574388f
Wolfgang Felbermeier <wf@felbermeier.com>
2018-04-13 09:45:17 +0200

    hmm.. looks like the build script is not executed in the build directory... strange

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get install -y librsvg2-dev curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
-npm install
 npm install -g prebuild
+cd /MyProgram
+npm install
 npm run prebuild
