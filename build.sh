ee8b679bbf16c2a0f71220442bacb725c38bf512
Wolfgang Felbermeier <wf@felbermeier.com>
2018-04-13 10:06:23 +0200

    debug build of librsvg

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 apt-get update
 apt-get install -y librsvg2-dev curl build-essential
 wget "https://github.com/GNOME/librsvg/archive/2.40.16.tar.gz" -O librsvg.tar.gz
+ls -la
 tar -xvzf librsvg.tar.gz
+ls -la
 cd librsvg-2.40.16
+ls -la
 apt-get install -y gcc make rustc cargo automake autoconf libtool gettext itstool libgdk-pixbuf2.0-dev libgirepository1.0-dev gtk-doc-tools git libgtk-3-dev libxml2-dev libcroco3-dev libcairo2-dev libpango1.0-dev
+ls -la
 ./autogen.sh
 make
 make install
