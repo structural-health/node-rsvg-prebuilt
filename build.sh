ab60c6ccae8a5bd6561c8ebea664a69ec0fb5bbb
Wolfgang Felbermeier <wf@felbermeier.com>
2018-04-13 09:40:44 +0200

    next test

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-apt-get install librsvg2-dev curl
+apt-get update
+apt-get install -y librsvg2-dev curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 npm install
 npm install -g prebuild
 npm run prebuild
