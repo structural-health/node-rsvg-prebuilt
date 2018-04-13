bff00e48c57065eba15f36f3f7e9374985a58ad0
Wolfgang Felbermeier <wf@felbermeier.com>
2018-04-13 09:59:45 +0200

    build librsvg by foot

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y librsvg2-dev curl build-essential
+wget "https://github.com/GNOME/librsvg/archive/2.40.16.tar.gz" -O librsvg.tar.gz
+tar -xvzf librsvg.tar.gz
+cd librsvg-2.40.16
+apt-get install -y gcc make rustc cargo automake autoconf libtool gettext itstool libgdk-pixbuf2.0-dev libgirepository1.0-dev gtk-doc-tools git libgtk-3-dev libxml2-dev libcroco3-dev libcairo2-dev libpango1.0-dev
+./autogen.sh
+make
+make install
+# node stuff
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 npm install -g prebuild
